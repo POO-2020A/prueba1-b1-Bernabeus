ae4fc46c1dae9444765ee51f8e20e43c8ae1109f
Bernabeus <58036212+Bernabeus@users.noreply.github.com>
2020-07-06 11:59:08 -0500

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,31 +1,42 @@
+
 import java.util.Scanner;
 
 /**
-Escribe un programa que pida una cadena al usuario y que revise si esta es un palindromo o no y terminar.
-
-Un palindromo significa que los caracteres de la cadena son los mismos al leer hacia adelante o hace atras.
-Debes ignorar los espacios, los acentos, puntuaciones y mayusculas y minusculas.
-
-Ejemplos de palindromos:
-Amor a Roma
-Bob
-Somos o no somos
-Acaso hubo buhos aca.
-Ana, la tacana catalana
-racecar
-was it a car or a cat I saw
-never odd or even
-rats live on no evil star
-
-El programa debe imprimir true si es un palindromo y false si no lo es.
-*/
+ * Escribe un programa que pida una cadena al usuario y que revise si esta es un
+ * palindromo o no y terminar.
+ *
+ * Un palindromo significa que los caracteres de la cadena son los mismos al
+ * leer hacia adelante o hace atras. Debes ignorar los espacios, los acentos,
+ * puntuaciones y mayusculas y minusculas.
+ *
+ * Ejemplos de palindromos: 
+ * Amor a Roma 
+ * Bob 
+ * Somos o no somos 
+ * Acaso hubo buhos aca.
+ * Ana, la tacana catalana 
+ * racecar 
+ * was it a car or a cat I saw 
+ * never odd or even 
+ * rats live on no evil star
+ *
+ * El programa debe imprimir true si es un palindromo y false si no lo es.
+ */
 class Palindrome {
-  public static void main(String[] args) {
-    Scanner inp = new Scanner(System.in);
-    System.out.print("In:");
-    String s = inp.nextLine();
-   	//write your code below
-   	
-   	
-  }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("In:");
+        String s = inp.nextLine();
+        s = s.replace(" ", "").replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u").replace(".", "").replace(",", "").toLowerCase();
+        String invertido = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            invertido = invertido + s.charAt(i);
+        }
+        if (invertido.equals(s)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
 }
